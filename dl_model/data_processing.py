cc5766eca933e30972365f8cfcef94f2008f0508
Mikhail Romanov <mikhail.romanov@aalto.fi>
2023-12-22 18:10:17 +0200

    moved one code block into function

--- dl_model/data_processing.py
+++ dl_model/data_processing.py
@@ -7,11 +7,14 @@ import json
 import numpy as np
 from matplotlib import pyplot as plt
 
+
 # Avoid OOM errors by setting GPU Memory Consumption Growth
-gpus = tf.config.experimental.list_physical_devices('GPU')
-for gpu in gpus:
-    tf.config.experimental.set_memory_growth(gpu, True)
-tf.config.list_physical_devices('GPU')
+def limit_gpu():
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    for gpu in gpus:
+        tf.config.experimental.set_memory_growth(gpu, True)
+    tf.config.list_physical_devices('GPU')
+
 
 # I moved images to test, val and train folders so images is empty,
 # if data is wanted then uncomment
