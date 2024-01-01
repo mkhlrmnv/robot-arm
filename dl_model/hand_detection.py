9baee96a002504a87154ec11e760658d7a974cab
Mikhail Romanov <mikhail.romanov@aalto.fi>
2024-01-01 17:59:57 +0200

    changed resolutions to match mac's webcamera

--- dl_model/hand_detection.py
+++ dl_model/hand_detection.py
@@ -8,34 +8,35 @@ facetracker = load_model('facetracker.h5')
 cap = cv2.VideoCapture(0)
 while cap.isOpened():
     _, frame = cap.read()
-    frame = frame[50:500, 50:500, :]
+    # frame = frame[50:500, 50:500, :]
 
     rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     resized = tf.image.resize(rgb, (256, 144))
 
     yhat = facetracker.predict(np.expand_dims(resized / 255, 0))
+    print(yhat)
     sample_coords = yhat[1][0]
 
     if yhat[0] > 0.5:
         # Controls the main rectangle
         cv2.rectangle(frame,
-                      tuple(np.multiply(sample_coords[:2], [640, 360]).astype(int)),
-                      tuple(np.multiply(sample_coords[2:], [640, 360]).astype(int)),
+                      tuple(np.multiply(sample_coords[:2], [1920, 1080]).astype(int)),
+                      tuple(np.multiply(sample_coords[2:], [1920, 1080]).astype(int)),
                       (255, 0, 0), 2)
         # Controls the label rectangle
         cv2.rectangle(frame,
-                      tuple(np.add(np.multiply(sample_coords[:2], [640, 360]).astype(int),
+                      tuple(np.add(np.multiply(sample_coords[:2], [1920, 1080]).astype(int),
                                    [0, -30])),
-                      tuple(np.add(np.multiply(sample_coords[:2], [640, 360]).astype(int),
+                      tuple(np.add(np.multiply(sample_coords[:2], [1920, 1080]).astype(int),
                                    [80, 0])),
                       (255, 0, 0), -1)
 
         # Controls the text rendered
-        cv2.putText(frame, 'face', tuple(np.add(np.multiply(sample_coords[:2], [640, 360]).astype(int),
-                                                [0, -5])),
+        cv2.putText(frame, 'right-hand', tuple(np.add(np.multiply(sample_coords[:2], [1920, 1080]).astype(int),
+                                                      [0, -5])),
                     cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
 
-    cv2.imshow('EyeTrack', frame)
+    cv2.imshow('HandTrack', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
