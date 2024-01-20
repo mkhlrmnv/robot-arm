21ebc2ce6d396cd3bdea251881ea527dee7d800a
Mikhail Romanov <mikhail.romanov@aalto.fi>
2024-01-20 14:55:33 +0200

    added center point calculations and desplaying of circle in the center

--- dl_model/hand_detection.py
+++ dl_model/hand_detection.py
@@ -14,10 +14,12 @@ while cap.isOpened():
     resized = tf.image.resize(rgb, (256, 144))
 
     yhat = handtracer.predict(np.expand_dims(resized / 255, 0))
-    print(yhat)
     sample_coords = yhat[1][0]
 
-    if yhat[0] > 0.5:
+    max_co = (1920 / 2, 1080 / 2)
+    min_co = max_co
+
+    if yhat[0] > 0.7:
         # Controls the main rectangle
         cv2.rectangle(frame,
                       tuple(np.multiply(sample_coords[:2], [1920, 1080]).astype(int)),
@@ -31,10 +33,19 @@ while cap.isOpened():
                                    [80, 0])),
                       (255, 0, 0), -1)
 
+        min_co = tuple(np.multiply(sample_coords[:2], [1920, 1080]).astype(int))
+        max_co = tuple(np.multiply(sample_coords[2:], [1920, 1080]).astype(int))
+
         # Controls the text rendered
         cv2.putText(frame, 'hand', tuple(np.add(np.multiply(sample_coords[:2], [1920, 1080]).astype(int),
                                                 [0, -5])),
                     cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+
+    centerX = int((max_co[0] + min_co[0]) / 2)
+    centerY = int((max_co[1] + min_co[1]) / 2)
+    print(f"Center X: {centerX} and center Y: {centerY}")
+
+    cv2.circle(frame, (centerX, centerY), 10, (0, 0, 255), 2)
 
     cv2.imshow('HandTrack', frame)
 
